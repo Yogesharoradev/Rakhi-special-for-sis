708e459d64b934aabfe15232e030339a2ee483fd
Yogesharoradev <159639855+Yogesharoradev@users.noreply.github.com>
2026-08-28 00:02:27 +0530

    feat: switch app to three-sibling experience

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import SiblingsExperience from './SiblingsExperience.jsx';
 import MusicPlaylist from './MusicPlaylist.jsx';
 import './memory-page.css';
 import './hero-overrides.css';
 
-function Root() {
-  return (
-    <>
-      <App />
-      <MusicPlaylist />
-    </>
-  );
-}
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>
-);
+function Root(){return <><SiblingsExperience/><MusicPlaylist/></>}
+createRoot(document.getElementById('root')).render(<React.StrictMode><Root/></React.StrictMode>);

f77834bb7d680f9e32fdc5292e5027d44a5178d2
Yogesharoradev <159639855+Yogesharoradev@users.noreply.github.com>
2026-08-28 00:05:09 +0530

    restore original app layout and keep playlist

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import SiblingsExperience from './SiblingsExperience.jsx';
+import App from './App.jsx';
 import MusicPlaylist from './MusicPlaylist.jsx';
 import './memory-page.css';
 import './hero-overrides.css';
 
-function Root(){return <><SiblingsExperience/><MusicPlaylist/></>}
-createRoot(document.getElementById('root')).render(<React.StrictMode><Root/></React.StrictMode>);
+function Root() {
+  return (
+    <>
+      <App />
+      <MusicPlaylist />
+    </>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);

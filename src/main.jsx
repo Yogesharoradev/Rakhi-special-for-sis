b09817698982fd8c2c4d692cb7fdb23282be86f0
Yogesharoradev <159639855+Yogesharoradev@users.noreply.github.com>
2026-08-27 23:44:30 +0530

    feat: wire two Rakhi songs to play sequentially

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import MusicPlaylist from './MusicPlaylist.jsx';
 import './memory-page.css';
 import './hero-overrides.css';
 
+function Root() {
+  return (
+    <>
+      <App />
+      <MusicPlaylist />
+    </>
+  );
+}
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );

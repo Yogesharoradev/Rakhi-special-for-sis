441fd6aac1d52ab53b17ad9d6e706b74956c5690
Yogesharoradev <159639855+Yogesharoradev@users.noreply.github.com>
2026-08-28 10:47:59 +0530

    Add final love section without changing existing page

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import MusicPlaylist from './MusicPlaylist.jsx';
+import LoveYouSection from './LoveYouSection.jsx';
 import './memory-page.css';
 import './hero-overrides.css';
+import './love-you.css';
 
 function Root() {
   return (
     <>
       <App />
+      <LoveYouSection />
       <MusicPlaylist />
     </>
   );
